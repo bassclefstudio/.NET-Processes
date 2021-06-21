9e06c0002111159f3dfbd619510f41dfd4fe2ea1
bassclefstudio <bassclefstudio-dev@outlook.com>
2021-06-20 20:42:30 -0400

    Switched testing to just powershell, never failing if powershell is not installed.

--- BassClefStudio.NET.Processes.Tests/ProcessTests.cs
+++ BassClefStudio.NET.Processes.Tests/ProcessTests.cs
@@ -13,7 +13,6 @@ namespace BassClefStudio.NET.Processes.Tests
         public async Task TestShellAsync()
         {
             string message = "Hello World!";
-            List<Exception> shellExs = new List<Exception>();
             string[] output = null;
 
             try
@@ -21,37 +20,14 @@ namespace BassClefStudio.NET.Processes.Tests
                 Console.WriteLine("Running in Powershell:");
                 output = await $"Write-Host \"{message}\"".PowerShellCoreAsync();
                 Console.WriteLine("Powershell call complete.");
+
+                Assert.AreEqual(1, output.Length, "Output is unexpected number of lines.");
+                Assert.AreEqual(message, output[0], "Output value is unexpected.");
             }
             catch (Exception ex)
             {
-                Console.WriteLine("Powershell failed.");
-                shellExs.Add(ex);
+                Console.WriteLine($"Powershell failed. {ex}");
             }
-
-            try
-            {
-                Console.WriteLine("Running in Bash:");
-                output = await $"printf \"{message}\\n\"".BashAsync();
-                Console.WriteLine("Bash call complete.");
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine("Bash failed.");
-                shellExs.Add(ex);
-            }
-
-            if (shellExs.Count == 2)
-            {
-                foreach (var ex in shellExs)
-                {
-                    Console.WriteLine($"error: {ex}");
-                }
-
-                Assert.Fail("One or more of the shell tests failed to complete.");
-            }
-
-            Assert.AreEqual(1, output.Length, "Output is unexpected number of lines.");
-            Assert.AreEqual(message, output[0], "Output value is unexpected.");
         }
     }
 }
